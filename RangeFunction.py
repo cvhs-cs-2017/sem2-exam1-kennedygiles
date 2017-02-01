3d03be791cbf1fbc23d5d82e5369144c1f8bf924
kennedygiles <kennedygiles250@gmail.com>
2017-02-01 11:25:21 -0800

    finished
    problems #1 and #2 were easy for me but, i have trouble on the third and
    the only way i could figure it somewhat out was to give the input value
    instead of it working with any number

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,10 +1,19 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
 
-
+x = 0
+for i in range(40,0,-1):
+    x = x + i
+    print (i)
 
 """Repeat the exercise but count by 5's"""
-
+x = 0
+for i in range(40,0,-5):
+    x = x + i
+    print(i)
 
 
 """Write a program that will count print all the multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
+n = input()
+    n = x + i
+    print(i)
