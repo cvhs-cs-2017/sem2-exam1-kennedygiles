daadafc12afe71afcd4a48739fb9e029a20027e3
kennedygiles <kennedygiles250@gmail.com>
2017-02-03 10:03:26 -0800

    problem #2

--- Function.py
+++ Function.py
@@ -10,3 +10,7 @@ print(x)
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+
+n = input()
+p = 3*n
+print(p)
