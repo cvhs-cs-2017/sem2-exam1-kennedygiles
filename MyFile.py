9b2c536dea619e77adde0dbebb11eaf54f4d9c2f
kennedygiles <kennedygiles250@gmail.com>
2017-02-03 10:41:21 -0800

    problems #1 & 2
    i feel like my answers are right and should work but my code is not
    running? i really don't know what to do..

---
+++ MyFile.py
@@ -0,0 +1,23 @@
+ #In MyFile.py, define the following functions:
+  # AddTen(n):  Adds 10 to the parameter n and returns the result
+  # DrawRectangle(Anyturtle, l, w):  Self Explanitory
+  # DrawPoly(Anyturtle, n):  Will draw a regular polygon with 'n' sides of
+      #* You should select the size of the polygon so that it always fits in the screen
+
+n = 0
+def AddTen(n):
+    x = n + 10
+    return x
+
+
+
+import turtle
+Anyturtle = turtle.Turtle()
+w = 10
+l = 10
+def DrawRectangle(Anyturtle, l, w,):
+    for i in range(2):
+        t.forward(w)
+        t.left(90)
+        t.forward(l)
+        t.left(90)
